82c931695df325a14f301b527e676a5fba13222a
choijihye <choijh92@redwoodk.com>
2024-08-08 16:28:45 +0900

    applicationEvent 사용 하여 Commit 후 produce/consume 처리

---
+++ main/java/com/fastcampus/kafkahandson/event/MyCdcApplicationEventListener.java
@@ -0,0 +1,27 @@
+package com.fastcampus.kafkahandson.event;
+
+import com.fastcampus.kafkahandson.model.MyModelConverter;
+import com.fastcampus.kafkahandson.producer.MyCdcProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class MyCdcApplicationEventListener {
+
+    private final MyCdcProducer myCdcProducer;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Async
+    public void transactionEventListenerAfterCommit(MyCdcApplicationEvent event) {
+        try {
+            myCdcProducer.sendMessage(MyModelConverter.toMessage(event.getId(), event.getMyModel(), event.getOperationType()));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- main/java/com/fastcampus/kafkahandson/service/MyServiceImpl.java
+++ main/java/com/fastcampus/kafkahandson/service/MyServiceImpl.java
@@ -2,12 +2,12 @@ package com.fastcampus.kafkahandson.service;
 
 import com.fastcampus.kafkahandson.data.MyEntity;
 import com.fastcampus.kafkahandson.data.MyJpaRepository;
+import com.fastcampus.kafkahandson.event.MyCdcApplicationEvent;
 import com.fastcampus.kafkahandson.model.MyModel;
 import com.fastcampus.kafkahandson.model.MyModelConverter;
 import com.fastcampus.kafkahandson.model.OperationType;
-import com.fastcampus.kafkahandson.producer.MyCdcProducer;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +19,7 @@ import java.util.Optional;
 public class MyServiceImpl implements MyService {
 
     private final MyJpaRepository myJpaRepository;
-    private final MyCdcProducer myCdcProducer;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     public List<MyModel> findAll() {
@@ -36,27 +36,20 @@ public class MyServiceImpl implements MyService {
     @Override
     @Transactional
     public MyModel save(MyModel myModel) {
-
         OperationType operationType = myModel.getId() == null ? OperationType.CREATE : OperationType.UPDATE;
-
         MyEntity entity = myJpaRepository.save(MyModelConverter.toEntity(myModel));
-        try {
-            myCdcProducer.sendMessage(MyModelConverter.toMessage(entity.getId(), MyModelConverter.toModel(entity), operationType));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error processing JSON for sendMessage", e);
-        }
+        MyModel resultModel = MyModelConverter.toModel(entity);
 
-        return MyModelConverter.toModel(entity);
+        applicationEventPublisher.publishEvent(new MyCdcApplicationEvent(this, resultModel.getId(), resultModel, operationType));
+
+        return resultModel;
     }
 
     @Override
     @Transactional
     public void delete(Integer id) {
         myJpaRepository.deleteById(id);
-        try {
-            myCdcProducer.sendMessage(MyModelConverter.toMessage(id, null, OperationType.DELETE));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Error processing JSON for sendMessage", e);
-        }
+
+        applicationEventPublisher.publishEvent(new MyCdcApplicationEvent(this, id, null, OperationType.DELETE));
     }
 }
